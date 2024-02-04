4d330d26580e6da6b5e7c36199b87173fb958241
Ammar Israr <ammarisrar28@gmail.com>
2024-02-04 15:15:35 +0500

    Added validations for Login CNIC username field

--- app/src/main/java/com/example/intelligentalert/Login.java
+++ app/src/main/java/com/example/intelligentalert/Login.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Login extends AppCompatActivity {
 
@@ -19,9 +24,26 @@ public class Login extends AppCompatActivity {
         Button loginButton = findViewById(R.id.login_btn);
         TextView registerLink = findViewById(R.id.register_link);
 
+        EditText username = findViewById(R.id.username);
+
         Intent registerIntent = new Intent(this, Register.class);
 
-        loginButton.setOnClickListener(v -> Log.d("Login Clicked", "Login Button was clicked"));
+        loginButton.setOnClickListener(v -> {
+            if (TextUtils.isEmpty(username.getText().toString())) {
+                username.setError("Username can not be empty. Please enter a valid CNIC.");
+                return;
+            }
+            // Compile regular expression
+            final Pattern pattern = Pattern.compile("\\d\\d\\d\\d\\d-\\d\\d\\d\\d\\d\\d\\d-\\d", Pattern.CASE_INSENSITIVE);
+            // Match regex against input
+            final Matcher matcher = pattern.matcher(username.getText().toString());
+            // Use results...
+            if(!matcher.matches()) {
+                username.setError("Invalid format entered. Make sure that the CNIC number is entered in the format 12345-1234567-1");
+                return;
+            };
+            Log.d("Login","Username worked!");
+        });
         registerLink.setOnClickListener(v -> startActivity(registerIntent));
     }
 }
